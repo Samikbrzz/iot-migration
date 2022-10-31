7a8cb2ff2d36e780c9ea79fe75f6a592ce5f4277
Samikbrzz <samik@mobiliz.com.tr>
2022-10-31 09:56:41 +0300

    Migrated sensor values from backup file.

--- migrate.js
+++ migrate.js
@@ -2,10 +2,11 @@ const client = require("./migration/postgresql/client");
 const {migrateModels} = require("./migration/postgresql/migrateModels");
 const {migrateSensorMappings} = require("./migration/postgresql/migrateSensorMappings");
 const {migrateDevices} = require("./migration/postgresql/migrateDevices");
-const {migrateSensorValues} = require("./migration/cassandra/migrateSensorValues");
 const {migrationUISettings} = require("./migration/postgresql/migrateUISettings");
 const {migrationCommands} = require("./migration/postgresql/migrateCommands");
 const Console = require("console");
+const {updateSequences} = require("./migration/postgresql/updateSequences");
+const {getSensorValuesFromFolder} = require("./migration/cassandra/migrateSensorValuesFromBackupFile");
 
 async function migrate() {
     try {
@@ -17,13 +18,15 @@ async function migrate() {
         await migrateDevices().then(Console.log("Devices migration completed."));
         await migrationCommands().then(Console.log("Command migration completed."));
         await migrationUISettings().then(Console.log("UI Settings migration completed."));
-        await migrateSensorValues().then(Console.log("Sensor values migration is starting..."));
+        await updateSequences().then(Console.log("Updated sequences."));
+        client.platformApiDb.end();
+        client.mobilizIotDb.end();
+        await getSensorValuesFromFolder().then(Console.log("Migrated sensor values."));
         await process.exit();
     } catch (e) {
         console.log("Error " + e.stack);
         process.exit();
     }
 }
-
 
 migrate();
