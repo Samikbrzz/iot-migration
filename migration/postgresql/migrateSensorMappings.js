383a45214b2848424baee2ea000027d09d464841
Samikbrzz <samik@mobiliz.com.tr>
2022-11-01 14:22:18 +0300

    Removed unused functions from sensorMappings js.

--- migration/postgresql/migrateSensorMappings.js
+++ migration/postgresql/migrateSensorMappings.js
@@ -66,40 +66,4 @@ async function insertSensorMappingResults(sensorMappingsRows, insertQuery) {
     }
 }
 
-function sensorMappingQueryForScala() {
-    return `
-        select ds.id, ds.model_id, 'scalar' as mapping_type, ds."element" as element_name, s.name as sensor_name, s.primitive as data_type,
-        null as true_label, null as false_label, null as formula, null::jsonb as "map",
-        ds.created_by , ds.created_at as created_date, 'migration' as modified_by, ds.updated_at as modified_date
-        from device_sensors_bak ds join sensors s on s.id = ds.sensor_id where ds.model_id is not null and s.protocol_type = 'ANALOG' and s.value_type = 'SCALAR' UNION 
-    `;
-}
-
-function sensorMappingQueryForFormula() {
-    return `
-        select ds.id, ds.model_id, 'formula' as mapping_type, ds."element" as element_name, s.name as sensor_name, s.primitive as data_type,
-        null as true_label, null as false_label, coalesce(ds.value, s.value)  as formula, null::jsonb as "map",
-        ds.created_by , ds.created_at as created_date, 'migration' as modified_by, ds.updated_at as modified_date
-        from device_sensors_bak ds join sensors s on s.id = ds.sensor_id where ds.model_id is not null and s.value_type = 'FORMULA' UNION 
-    `;
-}
-
-function sensorMappingQueryForDigital() {
-    return `
-        select ds.id, ds.model_id, 'digital' as mapping_type, ds."element" as element_name, s.name as sensor_name, 'BOOLEAN' as data_type,
-        cast(s."label"::json->abs(coalesce (ds.value,s.value)::int - 1) as text) as true_label, cast(s."label"::json->coalesce (ds.value,s.value)::int as text) as false_label,
-        null as formula, null::jsonb as "map", ds.created_by , ds.created_at as created_date, 'migration' as modified_by, ds.updated_at as modified_date
-        from device_sensors_bak ds join sensors s on s.id = ds.sensor_id where ds.model_id is not null and s.protocol_type = 'DIGITAL' UNION 
-    `;
-}
-
-function sensorMappingQueryForTabular() {
-    return `
-        select ds.id, ds.model_id, 'tabular' as mapping_type, ds."element" as element_name, s.name as sensor_name, s.primitive as data_type,
-        null as true_label, null as false_label, null as formula, ds.value::jsonb  as "map",
-        ds.created_by , ds.created_at as created_date, 'migration' as modified_by, ds.updated_at as modified_date
-        from device_sensors_bak ds join sensors s on s.id = ds.sensor_id where  ds.model_id is not null and s.value_type = 'TABULAR';
-    `;
-}
-
 module.exports = migrateSensorMappings;
